74f38066b3764cfdbc19558809beaf4054a0553d
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-11-24 15:27:56 +0600

    adding ui updating function

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,15 +15,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz: [String] = ["Four + Two equals to Six", "Nine - Six equals to Three", "Five + Four equals to Nine"]
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        
+        updateUI()
+}
+    func updateUI() {
+        if questionNumber <= quiz.count - 1 {
+            questionLabel.text = quiz[questionNumber]
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
     }
 }
-
